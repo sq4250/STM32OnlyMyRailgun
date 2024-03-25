b60cdc1daa2b22f1447c5460c8df81bd925d0426
sq4250 <3535687466@qq.com>
2024-03-25 17:35:17 +0800

    win!

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -16,10 +16,10 @@
 enum {Do = 0, Re = 2, Mi = 4, Fa = 5, So = 7, La = 9, Si = 11};
 
 template <int F0, int S>
-constexpr int PSC() { return (1000000 / (F0 * std::pow(2, S / 12))); }
+constexpr int PSC() { return (1000000 / (F0 * std::pow(2, S / 12.0f))); }
 
 template <int F0, int S, int N>
-void notes() {__HAL_TIM_SET_PRESCALER(&htim4, (PSC<F0, S>())); HAL_Delay(N);}
+void notes() { __HAL_TIM_SET_PRESCALER(&htim4, (PSC<F0, S>())); HAL_Delay(N); }
 
 namespace part_A {
     const int F0 = 1975.53;
@@ -90,7 +90,9 @@ namespace part_B {
 #define l5__ notes<F0, So - 12, T16>();
 #define l6__ notes<F0, La - 12, T16>();
 #define l7__ notes<F0, Si - 12, T16>();
-
+#define s2_  notes<F0, Re +  1, T8 >();
+#define b6_  notes<F0, La -  1, T8 >();
+#define sl6__ notes<F0, La - 11, T16>();
 int main()
 {
     init();
@@ -102,9 +104,43 @@ int main()
             using namespace part_A;
             for (int i = 0; i < 7; i++) { _0_ l6_ l6_ l5__ l6__ _0_ l6_ l5_ l5__ l6__ }
             _0_ l6_ l6_ l5__ l6__ _0_ h1_ _7_ _5_
+            _5_ _6__ _6__ _6_ _5_ _5_ _6__ _6__ _6_ _5_
+            _5_ _6__ _6__ _6_ _6_ _6_ _7_ h1_ h2_
+            _7 _5 h4 h2 h2_ ___ h2__ h2_ h3_ _0_ h1_ _7_ _5_
+            _5_ _6__ _6__ _6_ _5_ _5_ _6__ _6__  _6_ _5_
+            _5_ _6__ _6__ _6_ _6_ _6_ _7_ h1_ h2_
+            _6 _ _6_ _7_ h1_ h2_
+            _7 _5 _3 _5
+            _5_ _6__ _6__ _6_ h1_ _7_ _5__ _3__ _3_ _5_
+            _5_ _6__ _6__ _6 _6 _
+            _6 _ _ _
         }
         {
             using namespace part_B;
+            _1 _ _1_ l7_ _1_ _2_
+            _1 _ _1_ l7_ _1_ _2_
+            _3 _ _3_ _4_ _3_ _2_
+            _3_ _2_ _1_ _5_ _3 _
+            _1 _ _1_ l7_ _1_ _2_
+            _1 _ _1_ l7_ _1_ _2_
+            _3 l6 _2_ _1_ l7_ _1_
+            l7 __ l6_ l6 _
+            _3_ _3__ _3__ _3__ _3__ _3_ _3_ _3_ _2_ _1_
+            l7 l7_ _5__ _5__ _5 __ _5__ _5__
+            _5_ _4_ _3_ _2_ _4__ _3__ _3__ _2__ _2_ _1_
+            l7_ ___ l7__ l7_ l6_ _1 _
+            _1_ _1__ _1__ _1__ _1__ _1_ _1_ _1_ _2_ s2_
+            _2 _2_ sl6__ l6__ l6 _0_ l6_
+            l6 l6_ l5_ l6_ l7__ _1__ _1_ _1_
+            _1 __ l7__ l6__ l7 _
+            _5_ _0__ b6_ _0__ _5_
+        }
+        {
+            using namespace part_A;
+            _0_ h1_ _7_ _5_
+            _5_ _6__ _6__ _6_ _5_ _5_ _6__ _6__ _6_ _5_
+            _5_ _6__ _6__ _6_ _6_ _6_ _7_ h1_ h2_
+            _7 _5 h4 h2
         }
     }
 }
